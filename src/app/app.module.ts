4f7488424c477995b950e1719a7865f2f7339b56
quardz <adal@quardz.com>
2021-06-20 05:03:38 -0800

    fixed the web3modal

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,14 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+//import Web3 from "web3";
+//import WalletLink from "walletlink";
+
+//import { Web3Modal } from "@mindsorg/web3modal-ts";
+//import WalletLink from "walletlink";
+import Web3 from "web3";
+//import Web3Modal from "web3modal";
+
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -9,6 +17,7 @@ import { FooterComponent } from './components/footer/footer.component';
 import { StatsComponent } from './pages/stats/stats.component';
 import { DappComponent } from './pages/dapp/dapp.component';
 import { DashboardComponent } from './pages/dashboard/dashboard.component';
+
 import { HomeComponent } from './pages/home/home.component';
 import { EffectsModule } from '@ngrx/effects';
 import { StoreModule } from '@ngrx/store';
@@ -31,6 +40,7 @@ import { environment } from '../environments/environment';
   imports: [
     BrowserModule,
     AppRoutingModule,
+
     NgbModule,
     EffectsModule.forRoot([]),
     StoreModule.forRoot({}, {}),
--- src/app/pages/dapp/dapp.component.ts
+++ src/app/pages/dapp/dapp.component.ts
@@ -3,6 +3,30 @@ import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 
 
 
+ // Unpkg imports
+declare var Web3: any;
+declare var Web3Modal: any;
+declare var WalletConnectProvider: any;
+declare var Fortmatic: any;
+declare var evmChains: any;
+
+//const web3 = window.Web3;
+//const Web3Modal = window.Web3Modal.default;
+//const WalletConnectProvider = window.WalletConnectProvider.default;
+//const Fortmatic = window.Fortmatic;
+//const evmChains = window.evmChains;
+
+// Web3modal instance
+//let web3Modal
+
+// Chosen wallet provider given by the dialog window
+//let provider;
+
+
+// Address of the selected account
+//let selectedAccount;
+
+
 @Component({
     // eslint-disable-next-line @angular-eslint/component-selector
   selector: 'app-dapp',
@@ -14,9 +38,83 @@ export class DappComponent implements OnInit {
   name = 'World';
 
 
-  constructor() { }
+  constructor() { 
+
+  }
+
+  public async openWallet() {
+    var _WalletConnectProvider = WalletConnectProvider.default;
+    var _Web3Modal = Web3Modal.default;
+    
+    const providerOptions = {
+      walletconnect: {
+        package: _WalletConnectProvider,
+        options: {
+          infuraId: "434b1b1b6da24d7b9ea4b49287e72d8e",
+        }
+      }
+    };
+
+    
+    var web3Modal = new _Web3Modal({
+      cacheProvider: false, // optional
+      providerOptions, // required
+      disableInjectedProvider: false, // optional. For MetaMask / Brave / Opera.
+    });
+    
+    const provider = await web3Modal.connect();
+
+    console.log("Web3Modal instance is", provider);
+
+    
+  }
+
 
   ngOnInit(): void {
+
+
+
+/**
+ * Setup the orchestra
+ */
+
+ /*
+
+  console.log("Initializing example");
+  console.log("WalletConnectProvider is", WalletConnectProvider);
+  //console.log("Fortmatic is", Fortmatic);
+  //console.log("window.web3 is", window.web3, "window.ethereum is", window.ethereum);
+
+  // Check that the web page is run in a secure context,
+  // as otherwise MetaMask won't be available
+  if(location.protocol !== 'https:') {
+    // https://ethereum.stackexchange.com/a/62217/620
   }
+
+  // Tell Web3modal what providers we have available.
+  // Built-in web browser provider (only one can exist as a time)
+  // like MetaMask, Brave or Opera is added automatically by Web3modal
+  const providerOptions = {
+    walletconnect: {
+      package: WalletConnectProvider,
+      options: {
+        // Mikko's test key - don't copy as your mileage may vary
+        infuraId: "434b1b1b6da24d7b9ea4b49287e72d8e",
+      }
+    },
+  };
+
+  web3Modal = new Web3Modal({
+    cacheProvider: false, // optional
+    providerOptions, // required
+    disableInjectedProvider: true, // optional. For MetaMask / Brave / Opera.
+  });
+
+  console.log("Web3Modal instance is", web3Modal);
+
+  */
+}
+
+
 
 }
